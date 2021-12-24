db4b8351dafc66115a2baf39833f1c167a92e072
Aleksandr Petushenko <aleksandr.petushenko1@gmail.com>
2021-12-25 02:05:10 +0300

    Add my project

---
+++ src/test/java/com/moon/senla/educational_website/EducationalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.senla.educational_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
